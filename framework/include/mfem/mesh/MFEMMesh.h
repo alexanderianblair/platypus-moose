01af61141063346a3156b1547d208b649f96ef33
Alexander Blair <alexander.blair@ukaea.uk>
2025-09-08 16:35:11 +0100

    First port of MFEMCoupledMesh from Apollo

--- framework/include/mfem/mesh/MFEMMesh.h
+++ framework/include/mfem/mesh/MFEMMesh.h
@@ -93,6 +93,7 @@ private:
    */
   std::optional<std::string> _mesh_displacement_variable;
 
+protected: 
   /**
    * Smart pointers to mfem::ParMesh object. Do not access directly.
    * Use the accessors instead.
